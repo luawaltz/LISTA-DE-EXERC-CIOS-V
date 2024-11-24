242ab15ec8bc79a4d24fdf81841aea6d92770fca
lua <luannavital.almeida@gmail.com>
2024-11-24 14:10:13 -0300

    QUESTÃO 05.c

---
+++ QUESTÃO 05.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int mcd(int a, int b) {
+    if (b == 0) {
+        return a;
+    } else {
+        return mcd(b, a % b);
+    }
+}
+
+int main() {
+    int a, b;
+
+    printf("Digite dois números inteiros positivos: ");
+    scanf("%d %d", &a, &b);
+
+    int resultado = mcd(a, b);
+    printf("O Mínimo Comum Divisor de %d e %d é: %d\n", a, b, resultado);
+
+    return 0;
+}
